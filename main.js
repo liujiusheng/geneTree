7fc2b4fa40d9397407a1a6c8de5e10341ae46506
liujiusheng <891599396@qq.com>
2017-04-21 13:51:35 +0800

    '修复了部分错误'

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var rstlength = tree.length;
 for(var i=0; i<rstlength; i++){
 	var eachRst = tree[i].children;//每一组染色体数据
 	var rst = {};
-	var topHeight = 50;
+	var topHeight = 0;
 	rst.x= 20+width*i;
 	var length = eachRst.length;
 	var r = 10;
@@ -23,17 +23,17 @@ for(var i=0; i<rstlength; i++){
 		var gyY = topHeight+allHeight/4-allGenHeight/2+gyHeight*k;
 		html += '<rect x="'+rst.x+'" y="'+gyY+'" width="'+gyWidth+'" height="'+gyHeight+'" style="fill:#91C7AF;stroke-width:1;stroke:#eee;"/>';
 		var eachYZ = eachRst[k].children;//每一组因子
-		var length = eachYZ.length;
+		var yzlength = eachYZ.length;
 		var circlex = rst.x+100+gyWidth;
 		var circlex2 = rst.x+100+gyWidth;
 		var y2 = topHeight+r*k*4;
-		var y3 = topHeight+r*(k+1)*2;
+		var y3 = topHeight+r*(2+k*4);
 		html += '<line x1="'+(rst.x+gyWidth)+'" y1="'+(gyY+gyHeight/2)+'" x2="'+circlex+'" y2="'+y2+'" style="stroke:rgb(99,99,99);stroke-width:1"/>';
-		for(var j=0;j<length;j++){
+		for(var j=0;j<yzlength;j++){console.log(j);
 			if(j<9){
 				html += '<circle cx="'+circlex+'" cy="'+y2+'" r="'+r+'" fill="'+eachYZ[j].color+'"/>';
 				circlex = circlex+2*r;
-			}else{
+			}else if(j>8&&j<16){
 				html += '<circle cx="'+circlex2+'" cy="'+y3+'" r="'+r+'" fill="'+eachYZ[j].color+'"/>';
 				circlex2 = circlex2+2*r;
 			}
